126117a3a8d8667f1452b3e8a7d2447b62d53e16
Felipe Moreno Borges <felipe.b2014@gmail.com>
2023-09-23 16:27:08 -0300

    feat: note details

---
+++ app/src/main/java/br/com/devcapu/securenote/ui/note/NoteDetailsActivity.kt
@@ -0,0 +1,12 @@
+package br.com.devcapu.securenote.ui.note
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.devcapu.securenote.R
+
+class NoteDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_details)
+    }
+}
