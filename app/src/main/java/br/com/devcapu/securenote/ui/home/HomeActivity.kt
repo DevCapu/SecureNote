7ded1b6b2fb0a1727256542d9bdf3d01b603198b
devcapu <felipe.b2014@gmail.com>
2023-09-23 23:32:05 -0300

    feat: listen to save action

--- app/src/main/java/br/com/devcapu/securenote/ui/home/HomeActivity.kt
+++ app/src/main/java/br/com/devcapu/securenote/ui/home/HomeActivity.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.devcapu.securenote.R
 import br.com.devcapu.securenote.data.entity.Note
+import br.com.devcapu.securenote.ui.note.EXTRA_NOTE_ID
 import br.com.devcapu.securenote.ui.note.NoteDetailsActivity.Companion.getIntent
 
 class HomeActivity : AppCompatActivity(R.layout.activity_home) {
@@ -17,8 +18,6 @@ class HomeActivity : AppCompatActivity(R.layout.activity_home) {
 
         val notes = mutableListOf(
             Note(1, "This is the content of Note 1.", "Content", "", ""),
-            Note(1, "This is the content of Note 1.", "Content", "", ""),
-            Note(1, "This is the content of Note 1.", "Content", "", ""),
         )
 
         val adapter = NoteListAdapter(notes) {
@@ -27,5 +26,7 @@ class HomeActivity : AppCompatActivity(R.layout.activity_home) {
         recyclerView.adapter = adapter
     }
 
-    private fun goToDetails() = startActivity(getIntent(this))
+    private fun goToDetails() = startActivity(getIntent(this).apply {
+        putExtra(EXTRA_NOTE_ID, 1)
+    })
 }
--- app/src/main/java/br/com/devcapu/securenote/ui/note/NoteDetailsActivity.kt
+++ app/src/main/java/br/com/devcapu/securenote/ui/note/NoteDetailsActivity.kt
@@ -4,12 +4,41 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import br.com.devcapu.securenote.R
+
+const val EXTRA_NOTE_ID = "NOTE_ID"
 
 class NoteDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_details)
+
+        val noteId = intent.getIntExtra(EXTRA_NOTE_ID, 0)
+        Log.d("MINHA TAG", noteId.toString())
+        if (noteId == 0) return
+
+        //TODO:Search in viewModel note with id:
+        //TODO: Add observable
+        setSupportActionBar(findViewById<Toolbar>(R.id.note_details_activity_toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //TODO: Verify menu is for a new note or edit one
+        Log.d("MINHA TAG", "onCreateOptionsMenu")
+        menuInflater.inflate(R.menu.activity_note_details_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.edit -> {
+            true
+        }
+
+        else -> super.onOptionsItemSelected(item)
     }
 
     companion object {
