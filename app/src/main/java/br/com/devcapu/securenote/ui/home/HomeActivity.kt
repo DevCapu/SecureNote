e1541321f78b18aef5e865e8ccbd58516890f391
devcapu <felipe.b2014@gmail.com>
2023-09-23 23:37:14 -0300

    feat: fab redirect to a new note

--- app/src/main/java/br/com/devcapu/securenote/ui/home/HomeActivity.kt
+++ app/src/main/java/br/com/devcapu/securenote/ui/home/HomeActivity.kt
@@ -8,11 +8,17 @@ import br.com.devcapu.securenote.R
 import br.com.devcapu.securenote.data.entity.Note
 import br.com.devcapu.securenote.ui.note.EXTRA_NOTE_ID
 import br.com.devcapu.securenote.ui.note.NoteDetailsActivity.Companion.getIntent
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeActivity : AppCompatActivity(R.layout.activity_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        configureNotesList()
+        configureFAB()
+    }
+
+    private fun configureNotesList() {
         val recyclerView = findViewById<RecyclerView>(R.id.activity_home_notes_list)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
@@ -24,6 +30,13 @@ class HomeActivity : AppCompatActivity(R.layout.activity_home) {
             goToDetails()
         }
         recyclerView.adapter = adapter
+    }
+
+    private fun configureFAB() {
+        val floatingActionButton = findViewById<FloatingActionButton>(R.id.activity_home_fab)
+        floatingActionButton.setOnClickListener {
+            goToDetails()
+        }
     }
 
     private fun goToDetails() = startActivity(getIntent(this).apply {
